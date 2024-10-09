e150fc28756838299e15ce20f1626fb3b0e0dc75
Romankasper <roman_kaspryshyn@ukr.net>
2024-10-09 13:19:15 +0300

    innit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Products from "./components/products/products";
 
 
 function App() {
   return (
     <div>
-
+<Products/>
     </div>
   );
 }
